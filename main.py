3af0d5eb79e4bb8481b2abe58fc8fb095ba2a8a6
fernandosilva-eng <fs2347554@gmail.com>
2025-12-15 19:37:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-
+# Configuração inicial do banco de dados
 def iniciarBanco(conexao):
     print("\n--- INICIANDO CONFIGURACAO DO SISTEMA MOTO PECAS ---\n")
     cursor = conexao.cursor()
@@ -13,10 +13,11 @@ def iniciarBanco(conexao):
     )
     """
     cursor.execute(codigo_sql)
-    print("SUCESSO: Cofiguracao concluida.\n")
     conexao.commit()
+    print("SUCESSO: Cofiguracao concluida.\n") # Mostra que houve exito na criação do banco
+    
 
-
+# Exibe o estoque armazenado no banco de dados e filtra 
 def exibirEstoque(conexao):
     print("\n--- SISTEMA MOTO PECAS: RELATORIO GERAL ---\n")
     cursor = conexao.cursor()
@@ -36,10 +37,11 @@ def exibirEstoque(conexao):
         nome = peca[1]
         preco = peca[2]
 
-        if preco > 200:
+        if preco > 200: # Filtro simples
             print(f"-> ATENCAO: O {nome} custa R$ {preco:.2f}!")
 
 
+# Apaga um iten do banco através do seu id
 def deletar_peca(conexao, id_peca):
     cursor = conexao.cursor()
     print("--- REMOVER PECA DO SISTEMA ---")
@@ -48,11 +50,11 @@ def deletar_peca(conexao, id_peca):
     conexao.commit()
     print(f"\nSUCESSO: A peça de ID {id_peca} foi removida do banco de dados.")
 
-
+# Loop para fazer o cadastro das peças
 def cadastrar_peca(conexao):
     while True:
         cursor = conexao.cursor()
-        dados_peca = ler_dados_produto()
+        dados_peca = ler_dados_produto() # Chama uma função especial para ler os dados
         comando_sql = "INSERT INTO pecas (nome, preco, quantidade) VALUES (?, ?, ?)"
         cursor.execute(
             comando_sql, (dados_peca["nome"], dados_peca["preco"], dados_peca["qtd_peca"]))
@@ -62,7 +64,7 @@ def cadastrar_peca(conexao):
         if res == '0':
             break
 
-
+# Ler os dados com auxilio de uma função específica
 def ler_dados_produto():
     nome_peca = ler_entrada_texto("Digite o nome da peca: ")
     preco_peca = float(ler_entrada_numerico("Digite o preco (ex: 150. 50): "))
@@ -70,30 +72,33 @@ def ler_dados_produto():
     return {"nome": nome_peca, "preco": preco_peca, "qtd_peca": qtd_peca}
 
 
+# Função especialista em ler dados numericos e validalos
 def ler_entrada_numerico(texto, fim_intervalo=0, ini_intervalo=0):
     while True:
         try:
             entrada = int(input(texto))
-            if (entrada<ini_intervalo) or (fim_intervalo and entrada>fim_intervalo):
-                raise ValueError
+            if (entrada<ini_intervalo) or (fim_intervalo and entrada>fim_intervalo): # Verifica entrada é menor que o limite minimo ou se o 
+                raise ValueError                                                     #fim tem algum valor e se esse valor é maior que o limite final         
             return entrada
         except ValueError: 
             print("\nErro: O valor precisa ser numerico e maior que ZERO!\n")
 
 
+# Função especialista em ler dados de textos e validalos
 def ler_entrada_texto(texto):
     while True:
         try:
             entrada = input(texto).strip()
-            if (entrada.isdigit()) or (not entrada):
+            if (entrada.isdigit()) or (not entrada): # Verifica se é apenas numeros ou se não foi digitado nada
                 raise ValueError
             return entrada
         except ValueError:
             print("\nErro: O nome NÃO pode ter apenas numeros e tambem nao pode ser vazio\n")
 
 
+# Atualizar um iten do banco através do seu id
 def atualizarDados(conexao, id):
-    while True:
+    while True:             
             atualizacao = ler_entrada_numerico(
                 "\nO que voce quer atualizar?\n1. Entradas no Estoque\n2. Saidas do Estoque\n3. Preco\n4. Nome\n0. VOLTAR\nDigite uma opcao -> ", 4)
             if atualizacao<0 or atualizacao>4:
@@ -149,7 +154,7 @@ def atualizar_quantidade(conexao, id_peca, entrada_saida):
         conexao.commit()
         return 0
 
-
+# Primeiro menu de opções
 def menu():
     print("           Menu de Opcoes\n")
     print("1. Exibir Estoque\n2. Cadastrar no Estoque\n3. Atualizar Estoque\n4. Apagar do Estoque\n0. SAIR\n")
@@ -157,6 +162,7 @@ def menu():
     return res
 
 
+# Script principal
 def main():
     conexao = sqlite3.connect('estoque.db')
     iniciarBanco(conexao)
@@ -179,6 +185,7 @@ def main():
             id_da_peca=ler_entrada_numerico("\nDigite o ID da peca que deseja EXCLUIR para sempre: ")
             deletar_peca(conexao, id_da_peca)
 
+    # Encerrando a conexão com o banco
     conexao.close()
 
 
